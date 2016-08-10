08634d0bda6397688c610163ca7efb3530343117
oaz <oaz@oneandzero>
2016-08-11 00:01:14 +0900

    Fixed for production env

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Github Webhook
 
 See 'app/controllers/scripts'
+Add 'GITHUB_WEBHOOK_SECRET' to .env
--- app/controllers/scripts/install_from_git.sh
+++ app/controllers/scripts/install_from_git.sh
@@ -4,9 +4,13 @@
 APP_DIR="/var/www/html/oneandzeroWeb"
 cd $APP_DIR
 git pull origin master
-chown www-data:www-data tmp
-chown -R oaz:oaz db
-chown www-data:www-data log
+chown -R oaz:oaz db tmp log
 sudo -i -u oaz bash -c "cd $APP_DIR; bundle install"
-sudo -i -u oaz bash -c "cd $APP_DIR; rake db:migrate RAILS_ENV=development"
-chown -R www-data:www-data db
+sudo -i -u oaz bash -c "cd $APP_DIR; DISABLE_DATABASE_ENVIRONMENT_CHECK=1  rake db:drop RAILS_ENV=production"
+sudo -i -u oaz bash -c "cd $APP_DIR; rake db:migrate RAILS_ENV=production"
+sudo -i -u oaz bash -c "cd $APP_DIR; rake db:seed RAILS_ENV=production"
+chown -R www-data:www-data db tmp log
+
+chown -R oaz:oaz public
+sudo -i -u oaz bash -c "cd $APP_DIR; RAILS_ENV=production rake assets:clean assets:precompile"
+chown -R root:root public
